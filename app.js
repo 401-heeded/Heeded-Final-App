b78371f06552597d7ce1fcb3ad962f470fea8861
Lillian Gales <lilliangales@ymail.com>
2019-09-09 16:47:58 -0700

    testing pi

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ function facialRecognition (count) {
     return;
     }
     console.log('hello------------------------------------------');
-    console.log(`stdout yaw: ${stdout}`);
-    // console.log(`stdout pitch: ${stdout.FaceDetails[0].Pose.Pitch}`);
-    // console.log(`stdout eyes open: ${stdout.FaceDetails[0].EyesOpen.Value}`);
+    console.log(`stdout yaw: ${stdout.FaceDetails[0].Pose.Yaw}`);
+    console.log(`stdout pitch: ${stdout.FaceDetails[0].Pose.Pitch}`);
+    console.log(`stdout eyes open: ${stdout.FaceDetails[0].EyesOpen.Value}`);
     console.error(`stderr: ${stderr}`);
   });
 }
