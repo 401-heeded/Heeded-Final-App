1bee82b634c012d4cb721dda570440848793bf0b
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-09-09 10:33:10 -0700

    testing camera script

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { exec } = require('child_process');
+
+exec('ls -a', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
