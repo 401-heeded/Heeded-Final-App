c432c4650d0b645c80c4d20d9369e5d63c4fed20
Bradley Elliott <bgelliott206@gmail.com>
2019-09-09 13:35:09 -0700

    photos uploading to bucket

---
+++ s3/server.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const AWS = require('aws-sdk');//Chance- brining in SDK and uuid
+AWS.config.update({ region: 'us-west-2' });
+
+
+const uuid = require('uuid');//Chance
+const S3 = new AWS.S3();
+const fs = require('fs');
+
+const getFile = (file) => {
+    fs.readFile( file, (err, data) => {
+      if(err) { throw err; }
+      const params = {
+        Bucket: "spike-test2",
+        Key:"IMG_3221.jpg", //Chance- name of the S3 bucket
+        ACL: "public-read-write",
+        Body: data,  //Chance- name of the photo in the bucket
+      };
+      S3.putObject(params, function (err, data) {
+        if (err) console.log(err, err.stack);
+        else console.log(data);
+      })
+    })
+};
+
+getFile('./photo/IMG_3221.jpg')
