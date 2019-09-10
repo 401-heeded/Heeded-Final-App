98eed279fe674ccda1238406207d9d762961c7fa
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-09-10 16:46:36 -0700

    fix merging

--- front-end/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const rekognition = require('../rekognition');
-
-$('#start').on('click', function() {
-  console.log('hi you started');
-  startRekognition();
-  // $.ajax('/rekognition')
-});
