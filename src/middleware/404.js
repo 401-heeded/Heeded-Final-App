db438e237df8c0572df1d872337969d45d7b2fe0
Evan Brecht-Curry <starfoxz13@gmail.com>
2019-09-12 10:49:49 -0700

    removing zombie code

--- src/middleware/404.js
+++ src/middleware/404.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
 module.exports = (req, res, next) => {
   let error = { error: 'Resource Not Found' };
   res.statusCode = 404;
